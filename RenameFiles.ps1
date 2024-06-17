28ce0ed885ee5fa9094b495ca2b7044191b743ca
Joshua Paul Woods <jpw1991@gmail.com>
2024-06-18 05:47:16 +1000

    improve powershell scripts and rename folder

--- RenameFiles.ps1
+++ RenameFiles.ps1
@@ -1,9 +1,7 @@
-param (
-    [string]$oldstring,
-    [string]$newstring
-)
+$oldstring = $args[0]
+$newstring = $args[1]
 
-if ($PSCmdlet.MyInvocation.BoundParameters.Count -ne 2) {
+if (($oldstring -eq "") -OR ($newstring -eq "")) {
     Write-Host "Usage: .\script.ps1 <old string> <new string>"
     exit 1
 }
@@ -11,12 +9,12 @@ if ($PSCmdlet.MyInvocation.BoundParameters.Count -ne 2) {
 $directory = "."
 
 # Replace string in file and folder names
-Get-ChildItem -Path $directory -Recurse -Force -Exclude .git | Where-Object { $_.Name -like "*$oldstring*" } | ForEach-Object {
+Get-ChildItem -Path $directory -Recurse -Exclude *git | Where-Object { $_.Name -like "*$oldstring*" } | ForEach-Object {
     $newName = $_.Name -replace [regex]::Escape($oldstring), $newstring
-    Rename-Item -Path $_.FullName -NewName $newName -Force
+    Rename-Item -Path $_.FullName -NewName $newName
 }
 
 # Replace string in file contents
-Get-ChildItem -Path $directory -Recurse -File -Force -Exclude .git | ForEach-Object {
+Get-ChildItem -Path $directory -Recurse -File -Exclude *git | ForEach-Object {
     (Get-Content -Path $_.FullName) -replace [regex]::Escape($oldstring), $newstring | Set-Content -Path $_.FullName
 }
--- RenameTemplate.ps1
+++ RenameTemplate.ps1
@@ -1,7 +1,3 @@
-# Make sure the RenameFiles.ps1 script is in the same directory or provide the correct path to it
-$scriptPath = ".\RenameFiles.ps1"
-
-# Invoke the RenameFiles.ps1 script with different arguments
-& $scriptPath "ValheimMeshReplacerTemplate" "MyMod"
-& $scriptPath "valheimmeshreplacertemplate" "mymod"
-& $scriptPath "valheim-mesh-replacer-template" "my-mod"
+.\RenameFiles.ps1 "MyMod" "MyMod"
+.\RenameFiles.ps1 "MyMod" "mymod"
+.\RenameFiles.ps1 "valheim-mesh-replacer-template" "my-mod"
